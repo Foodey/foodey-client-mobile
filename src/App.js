f3ed56710cccf62993a026397985a9039f7ad43f
ngphth1312 <ngphth1312@gmail.com>
2023-11-21 14:02:15 +0700

    Done import AsyncStorage and using it to make OnBoardingScreens only launch once at the first time opening the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -10,16 +10,30 @@ import AuthenticateStackNavigator from './screens/authenticate/SignInUpScreen';
 const MainStack = createStackNavigator();
 
 function App() {
+  const [isAppFirstLaunch, setIsAppFirstLaunch] = useState(null);
+
+  useEffect(async () => {
+    const appData = await AsyncStorage.getItem('isAppFirstLaunch');
+    if (appData == null) {
+      setIsAppFirstLaunch(true);
+      AsyncStorage.setItem('isAppFirstLaunch', 'false');
+    } else {
+      setIsAppFirstLaunch(false);
+    }
+  }, []);
+
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#FFFFFF' }}>
-      <StatusBar backgroundColor="#FFFFFF" />
-      <NavigationContainer>
-        <MainStack.Navigator screenOptions={{ headerShown: false }}>
-          <MainStack.Screen name="Intro" component={IntroStackNavigator} />
-          <MainStack.Screen name="Authenticate" component={AuthenticateStackNavigator} />
-        </MainStack.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
+    isAppFirstLaunch != null && (
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#FFFFFF' }}>
+        <StatusBar backgroundColor="#FFFFFF" />
+        <NavigationContainer>
+          <MainStack.Navigator screenOptions={{ headerShown: false }}>
+            {isAppFirstLaunch && <MainStack.Screen name="Intro" component={IntroStackNavigator} />}
+            <MainStack.Screen name="Authenticate" component={AuthenticateStackNavigator} />
+          </MainStack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    )
   );
 }
 
