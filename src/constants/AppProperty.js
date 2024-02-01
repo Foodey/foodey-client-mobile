f30b6fa109176d16b8cf646b6bc27a2be6d83ffe
ngphth1312 <ngphth1312@gmail.com>
2024-02-01 22:28:51 +0700

    feat: adding new constants to serve API calls and AsyncStorage jobs

---
+++ src/constants/AppProperty.js
@@ -0,0 +1,6 @@
+const AppProperty = {
+  APP_NAME: 'FOODEY',
+  FOODEY_API_URL: 'http://10.0.2.2:8080/api',
+};
+
+export default AppProperty;
---
+++ src/constants/StorageKey.js
@@ -0,0 +1,7 @@
+const StorageKey = {
+  USER_INFOS: 'userInfos',
+  ACCESS_TOKEN: 'accessToken',
+  REFRESH_TOKEN: 'refreshToken',
+};
+
+export default StorageKey;
